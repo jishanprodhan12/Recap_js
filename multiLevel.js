285a68f840ad93cae3d0221fdee14f74d67498e8
Jishan <mdjishanprodhan132@gamil.com>
2025-06-13 23:16:09 -0500

    morobi er discount

--- multiLevel.js
+++ multiLevel.js
@@ -9,19 +9,29 @@
  * ==> motaMotiMorobi er age 60
  * ==> old er age 75
  */
-let age = 18 ;
+let age = 73 ;
 let price = 500 ;
 const  baccha = 12 ;
 const motaMotiMorobi = 50 ;
 const morobi = 60 ;
 const old = 75 ;
+console.log("Regular biriany price "+price+" tk");
 if(age <= baccha){
     console.log("tumar tk lagbo nah tumi free biriany paiba ");
 }
-else if(age >= motaMotiMorobi){
+// 5% discount 
+else if(age >=motaMotiMorobi && age < morobi){
     let dicsount = price * 5 / 100 ;
     let payPrice = price - dicsount ;
     console.log("discount 5% a hoilo : "+dicsount);
+    console.log("biriany er dam : "+payPrice+" tk");
+    
+}
+// 10% discount 
+else if(age >= morobi){
+    let dicsount = price * 10 / 100 ;
+    let payPrice = price - dicsount ;
+    console.log("discount 10% a hoilo : "+dicsount);
     console.log("biriany er dam : "+payPrice+" tk");
 }
 else {
