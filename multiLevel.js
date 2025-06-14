8dca4baecb02aeea6a3145b2ce37c4580e284b8f
Jishan <mdjishanprodhan132@gamil.com>
2025-06-13 23:03:47 -0500

    motamoti moribi er dam and discount

--- multiLevel.js
+++ multiLevel.js
@@ -4,17 +4,26 @@
  * 2. jodi motaMotiMorobi hoy taile 5% discount 
  * 3. jodi morobi hoy taile 10% discount 
  * 4. jodi old person hoy taile free 
+ * ==> baccha er age 12 
+ * ==> morobi er age 50
+ * ==> motaMotiMorobi er age 60
+ * ==> old er age 75
  */
 let age = 18 ;
 let price = 500 ;
 const  baccha = 12 ;
-const motaMotiMorobi = 45 ;
-const morobi = 55 ;
+const motaMotiMorobi = 50 ;
+const morobi = 60 ;
 const old = 75 ;
 if(age <= baccha){
     console.log("tumar tk lagbo nah tumi free biriany paiba ");
 }
-
+else if(age >= motaMotiMorobi){
+    let dicsount = price * 5 / 100 ;
+    let payPrice = price - dicsount ;
+    console.log("discount 5% a hoilo : "+dicsount);
+    console.log("biriany er dam : "+payPrice+" tk");
+}
 else {
     console.log('biriany er dam '+price+' tk');
 }
