1b717ecdb5c5162f5de1c674a61aef6419f67846
Jishan <mdjishanprodhan132@gamil.com>
2025-06-13 22:16:57 -0500

    jodi baccha hoy taile free

--- multiLevel.js
+++ multiLevel.js
@@ -1,2 +1,13 @@
 let age = 18 ;
-const  
+let price = 500 ;
+const  baccha = 12 ;
+const motaMotiMorobi = 45 ;
+const morobi = 55 ;
+const old = 75 ;
+if(age <= baccha){
+    console.log("tumar tk lagbo nah tumi free biriany paiba ");
+}
+
+else {
+    console.log('biriany er dam '+price+' tk');
+}
