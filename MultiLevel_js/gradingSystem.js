93ecfca91e96c50ca1fec18851f9313411662212
Jishan <mdjishanprodhan132@gamil.com>
2025-06-15 23:20:51 -0500

    grading system.js

--- MultiLevel_js/gradingSystem.js
+++ MultiLevel_js/gradingSystem.js
@@ -12,3 +12,25 @@ C if 60–69
 
 F if < 60
 */
+const  mark = 40 ;
+if(mark >= 0 && mark <=100){
+    if(mark >= 90 ){
+        console.log("congratulations you got A+");
+    }
+    else if(mark >=80 && mark <90){
+        console.log("congratulations you got A");
+    }
+    else if(mark >=70 && mark <80){
+        console.log("congratulations you got B");
+    }
+    else if(mark >=60 && mark <70){
+        console.log("congratulations you got C");
+    }
+    else{
+        console.log("You have Failed");
+        console.log("Better Luck next time");
+    }
+}
+else{
+    console.log("Invalid number ");
+}
